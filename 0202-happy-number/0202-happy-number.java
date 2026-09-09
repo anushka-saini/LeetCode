658203dc886752f520c9ed5d4639a381ba6f548e
Anushka Saini <anushkaaa2022@gmail.com>
2026-09-09 19:26:00 +0530

    Time: 1 ms (77.62%), Space: 42.5 MB (50.75%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int summ(int n){
+        int sum=0;
+
+        while(n>0){
+            int d=n%10;
+            sum=sum+(d*d);
+            n=n/10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast!=1){
+            slow=summ(slow);
+            fast=summ(summ(fast));
+            if(fast==1){
+                return true;
+            }
+            if(slow==fast){
+                return false;
+            }
+        }
+        return true;
+    }
+}
