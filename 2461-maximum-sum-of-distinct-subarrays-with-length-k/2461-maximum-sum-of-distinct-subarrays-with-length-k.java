6925d81c383b882c6382f721162710a8b8c78d44
Anushka Saini <anushkaaa2022@gmail.com>
2026-09-22 21:28:40 +0530

    Time: 85 ms (5.12%), Space: 108.7 MB (5.05%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,38 +1,48 @@
 class Solution {
-    public long maximumSubarraySum(int[] arr, int k) {
+    public long maximumSubarraySum(int[] nums, int k) {
+        long sum =0;
+        long max=0;
+        Map<Integer,Integer> map= new HashMap<>();
+        int dups=0;
+        for(int i=0;i<k;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],0);
+            }
+            map.put(nums[i],map.get(nums[i])+1);
+            sum+=nums[i];
+            if(map.get(nums[i])>1){
+                dups+=1;
 
-        int n = arr.length;
-
-        long window_sum = 0;
-        long max_ans = 0;
-
-        HashSet<Integer> set = new HashSet<>();
-
-        int left = 0;
-
-        for (int right = 0; right < n; right++) {
-
-
-            while (set.contains(arr[right])) {
-                set.remove(arr[left]);
-                window_sum -= arr[left];
-                left++;
             }
 
-            set.add(arr[right]);
-            window_sum += arr[right];
-
-            if (right - left + 1 > k) {
-                set.remove(arr[left]);
-                window_sum -= arr[left];
-                left++;
-            }
-
-            if (right - left + 1 == k) {
-                max_ans = Math.max(max_ans, window_sum);
-            }
         }
+        if(dups==0){
+            max=Math.max(max,sum);
+        }
+        for (int i=k;i<nums.length;i++){
+            int numstoadd=nums[i];
+            int numstoremove=nums[i-k];
+            if(!map.containsKey(numstoadd)){
+                map.put(numstoadd,0);
 
-        return max_ans;
+            }
+            map.put(numstoadd,map.get(numstoadd)+1);
+            if (map.get(numstoadd)>1){
+                dups+=1;
+            }
+        
+            sum+=numstoadd;
+            if(map.get(numstoremove)>1){
+                dups=dups-1;
+            }
+            map.put(numstoremove,map.get(numstoremove)-1);
+            sum-=numstoremove;
+            if(dups==0){
+                max=Math.max(sum,max);
+            }
+
+        }
+        return max;
     }
 }
+
